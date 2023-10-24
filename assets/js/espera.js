56193009a60d46c67f958967e2a4a9480b9aa373
aylin <fernandezaylin614qgmail.com>
2023-10-24 15:41:47 -0300

    archivos 2

---
+++ assets/js/espera.js
@@ -0,0 +1,30 @@
+/*----- PARTE DEL POPUP DEL PERFIL DE PERSONAJE  */
+let close = document.getElementById("closebtn");
+let open = document.getElementById("btnperfil");
+let popupcontainer = document.getElementById("Pp");
+ open.addEventListener("click", () => {
+    popupcontainer.style.display = "block";
+});
+close.addEventListener("click", () => {
+    popupcontainer.style.display = "none";
+});
+popupcontainer.addEventListener("click", (e) => {
+    if (e.target === popupcontainer) {
+        popupcontainer.style.display = "none";
+    }
+});
+/*----- PARTE DEL POPUP DEL  */
+let abrir = document.getElementById("btn_mas");
+let cerrar= document.getElementById("btnclosee");
+let contenido= document.getElementById("popupContainer");
+abrir.addEventListener("click",()=>{
+contenido.style.display="block";
+});
+cerrar.addEventListener("click",()=>{
+    contenido.style.display="none";
+});
+contenido.addEventListener("click", (e)=>{
+    if (e.target === contenido) {
+        contenido.style.display="none";
+    }
+});
---
+++ assets/js/game.js
@@ -0,0 +1,4 @@
+let btnsiguiente = document.getElementById("siguiente");
+//  en esta parte se supone que tiene que hacer el botn para ir a otra pagina que te muestre dos casoso 
+//posible: si supero el puntaje se muestre un html que diga felicidades y de la pista, el otro caso que 
+//cuando presione el boton y no supero el puntaje entonces muestre un html que muestre que perdio 
